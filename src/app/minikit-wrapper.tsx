5d9bfd10c9ed47e8381cadf55fbe730fa0b3f6d2
tomaspiskule <tomaspiskule@seznam.cz>
2025-05-31 16:13:34 +0200

    progress
    progress

--- src/app/minikit-wrapper.tsx
+++ src/app/minikit-wrapper.tsx
@@ -1,6 +1,7 @@
 'use client'
 
-import { MiniKitProvider } from '@worldcoin/minikit-js/minikit-provider'
+//import { MiniKitProvider } from '@worldcoin/minikit-js/minikit-provider'
+import { MiniKitProvider } from '@worldcoin/minikit-react'
 import { ReactNode } from 'react'
 
 export default function MiniKitClientWrapper({
